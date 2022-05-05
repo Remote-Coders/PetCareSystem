cf8bb246bdd0fad66987ce42d0d3eb89ae67bca8
L.A.M.D. Perera <71142129+IT20636692@users.noreply.github.com>
2022-05-05 08:44:21 +0530

    UI Updated(pet)

--- app/src/main/java/com/example/remotecoders/MainActivity2.java
+++ app/src/main/java/com/example/remotecoders/MainActivity2.java
@@ -2,13 +2,28 @@ package com.example.remotecoders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button pets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        pets = findViewById(R.id.btn_pets);
+        pets.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                                          Intent intent = new Intent(MainActivity2.this,DashActivity2.class);
+                                          startActivity(intent);
+                                      }
+                                  }
+
+
+        );
     }
 }
