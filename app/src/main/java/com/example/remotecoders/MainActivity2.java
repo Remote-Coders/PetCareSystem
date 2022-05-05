3890550eab8b63f5636534d027fa4601637aa93e
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-05-05 11:27:55 +0530

    UI updated(care)

--- app/src/main/java/com/example/remotecoders/MainActivity2.java
+++ app/src/main/java/com/example/remotecoders/MainActivity2.java
@@ -2,13 +2,30 @@ package com.example.remotecoders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button adsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        adsBtn = findViewById(R.id.btn_Ads);
+
+        adsBtn.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent = new Intent(MainActivity2.this,AdminActivityAds.class);
+                  startActivity(intent);
+              }
+          }
+
+
+        );
     }
 }
