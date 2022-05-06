f355697f5f475a94490007722eb673957c17bd7e
PrathibhaSathyanjalee <88728787+IT20636524@users.noreply.github.com>
2022-05-06 09:28:21 +0530

    Third commit-Add food(Food)

--- app/src/main/java/com/example/remotecoders/MainActivity2.java
+++ app/src/main/java/com/example/remotecoders/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.remotecoders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button foodbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        foodbtn=findViewById(R.id.btn_view_food);
+
+        foodbtn.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent intent = new Intent(MainActivity2.this,MainActivity4.class);
+                  startActivity(intent);
+              }
+          }
+          );
     }
 }
--- app/src/main/java/com/example/remotecoders/MainActivity9.java
+++ app/src/main/java/com/example/remotecoders/MainActivity9.java
@@ -2,13 +2,32 @@ package com.example.remotecoders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity9 extends AppCompatActivity {
-
+    ImageView dogfoodbtn;
+    ImageView catfoodbtn;
+    ImageView rabbitfoodbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main9);
+        dogfoodbtn=findViewById(R.id.dogfood_1);
+        catfoodbtn=findViewById(R.id.dogfood_2);
+        rabbitfoodbtn=findViewById(R.id.dogfood_3);
+
+        dogfoodbtn.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   Intent intent = new Intent(MainActivity9.this,MainActivity10.class);
+                   startActivity(intent);
+               }
+           }
+        );
+
     }
 }
